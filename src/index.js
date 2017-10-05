07f52f02ba37241ce2e72ee35b5345b46e715a47
devori.lee <devori.lee@gmail.com>
2017-10-05 16:13:44 +0900

    [bug fix] incorrect method. use set to use method when bodyParser is inserted

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import privateRouter from './router/private'
 
 const app = express()
-app.set(bodyParser.json());
+app.use(bodyParser.json());
 app.use('/api/v1/private', privateRouter);
 
 const PORT = process.env.PORT || 7000;
